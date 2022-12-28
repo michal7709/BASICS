7b51d2e032e9767be2c6038cdb9ea537a5ed6c15
michał <michalfabisiak@gmail.com>
2022-12-28 21:54:47 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Zakres tematyczny ( ćwiczenia ):
+# 1. Typy danych ( Lists, Dictionaries, Tuples)
+# 2. Pętle (for, case)
+# 3. Funkcje
+
+
+
+# 1. Listy
+
+new = []
+new1 = ["jabłko", "malina", "groszek"]
+
+for i in new1:
+    print(i)
+
+new2 = new1.sort()
+print(new2)
+
+new1.extend(["gruszka"])
+print(new1)
+
+
